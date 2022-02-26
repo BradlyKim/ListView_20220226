fb77b3c5b578a66682a063adb65f039d9d6fec69
BradlyKim <kjh5864@naver.com>
2022-02-26 15:35:27 +0900

    ArrayList에 학생 데이터 추가

--- app/src/main/java/com/neppplus/listview_20220226/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20220226/MainActivity.kt
@@ -2,10 +2,25 @@ package com.neppplus.listview_20220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listview_20220226.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
+    
+//     학생 목록을 담을 그릇
+    val mStudentList = ArrayList<StudentData>( )
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        
+        mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+        mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+        mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+        mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+        mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+        mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+        mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+        mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+
+        }
 }
