6c644a6b37f91c74ba5104c2aa02f30ea9b24214
BradlyKim <kjh5864@naver.com>
2022-02-26 15:44:19 +0900

    StudentAdapter를 메인화면에서 실제로 사용할 준비  - lateinit var로 변수 생성  - StudentAdapter (재료들)로 나중에 대입
    리스트뷰에 mAdapter 연결

--- app/src/main/java/com/neppplus/listview_20220226/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20220226/MainActivity.kt
@@ -2,12 +2,17 @@ package com.neppplus.listview_20220226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.neppplus.listview_20220226.adapters.StudentAdapter
 import com.neppplus.listview_20220226.datas.StudentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     
 //     학생 목록을 담을 그릇
     val mStudentList = ArrayList<StudentData>( )
+
+    lateinit var mAdapter: StudentAdapter
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,11 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
         mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
         mStudentList.add( StudentData("김정환", 1981, "서울시 중랑구") )
+
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+
+        studentListView.adapter = mAdapter
+
 
         }
 }
